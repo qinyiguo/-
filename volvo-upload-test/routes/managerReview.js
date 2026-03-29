cd4280e3e1b6d9d0abb8edf6645b99f8afaf1a9f
Bruce Kuo <bruce800606@gmail.com>
2026-03-29 20:37:35 +0800

    Update managerReview.js

--- volvo-upload-test/routes/managerReview.js
+++ volvo-upload-test/routes/managerReview.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const pool    = require('../db');
+const pool = require('../db/pool');
 
 // GET /api/manager-review?period=202603
 router.get('/', async (req, res) => {
